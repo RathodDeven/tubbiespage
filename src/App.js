983da5132116745ef08d8f6591ae41a5fd65d66a
RathodDeven <devenrathodrd@gmail.com>
2022-03-06 08:23:53 +0530

    tubbieverse

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <button className='enterBtn' onClick={onClick}>
-        Enter TubbiesVerse
+        Enter TubbieVerse
       </button>
     </div>
   );
