2be600d20545da31899fa1029bd932166f198f8c
RathodDeven <devenrathodrd@gmail.com>
2022-03-07 17:57:59 +0530

    public room access and icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const onClick = () => {
-    window.location.href = "https://interality.io/HFwSvX9/tubbieverse";
+  const [rooms,setRooms] = useState([]);
+
+  useEffect(() => {
+    const foo = async() => {
+      const data = await fetch("https://interality.io/api/v1/media/search?source=rooms&filter=public&cursor=0",{
+        headers: {"content-type": "application/json"},
+        method: "GET"
+      }).then(r => r.json());
+      console.log("publicRooms from reticulum",data.entries);
+
+      setRooms(data.entries);
+    }
+    foo();
+  },[])
+
+  const availableRoom = () => {
+    console.log("all ROOms",rooms);
+    if(rooms.length === 0){
+      return;
+    }
+    for (let i = 0; i < rooms.length; i++) {
+      let room = rooms[i];
+      if((room.lobby_count + room.member_count) < 60){
+            console.log("selected room ",room.url)
+            return room.url;
+      }
+    } 
+
+    alert("Wait for a while and try again");
+    // for(const room in rooms){
+    //   console.log("room",room);
+    //   if((room.lobby_count + room.member_count) <= 3){
+    //     console.log("selected room ",room.url)
+    //     return room.url;
+    //   }
+    // }
+  }
+  const onClick = async () => {
+    const roomLink = availableRoom();
+    console.log("roomLink",roomLink);
+    // window.location.href = roomLink;
+    // window.location.href = "https://interality.io/HFwSvX9/tubbieverse";
   }
   return (
     <div className="App">
